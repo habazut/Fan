660dde2a6496ef2496efe746d942220b23f05df2
Harald Barth <haba@kth.se>
2024-10-20 22:18:30 +0200

    eeprom changes

--- Fan.ino
+++ Fan.ino
@@ -5,6 +5,7 @@
 #include <string.h>
 #define TIMER1_A_PIN   9
 #define TIMER1_B_PIN   10
+#define MAGIC "Fan1.0"
 
 struct MyEE {
     char magic[8];
@@ -12,48 +13,61 @@ struct MyEE {
 };
 struct MyEE myee;
 long int dutyCycle = 0;
+byte led;
+
+void setSpeed() {
+  Timer1.pwm(TIMER1_A_PIN, dutyCycle * 1024 / 100);
+}
+
+void writeEEPROM() {
+  strcpy(myee.magic, MAGIC);
+  myee.pwm=dutyCycle;
+  EEPROM.put(0,myee);
+  for(int n=0; n<10 ; n++) {
+    delay(100);
+    digitalWrite(LED_BUILTIN, led = !led);
+  }
+}
 
 void setup(void) {
 
     Serial.begin(115200);
-    EEPROM.get(0, myee);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Timer1.initialize(500);  // 500us = 2kHz, 40 us = 25 kHz
 
+    EEPROM.get(0, myee);
     Serial.println("");
     Serial.print("EEPROM read ");
     Serial.print(myee.magic);
     Serial.print(" speed=");
-    if (strcmp(myee.magic, "Fan1.0") == 0) {
-	dutyCycle = myee.pwm;
+    if (strcmp(myee.magic, MAGIC) == 0) {
+        dutyCycle = myee.pwm;
 	Serial.print(myee.pwm);
-	Serial.println("%");
+        Serial.println("%");
     } else {
-	Serial.println(" ignored");
+        Serial.println(" ignored");
+        dutyCycle = 10;
+	writeEEPROM();
     }
-    strcpy(myee.magic,"Fan1.0");
-    myee.pwm=dutyCycle;
-
-    Timer1.initialize(500);  // 500us = 2kHz, 40 us = 25 kHz
+    Serial.print("Start PWM Fan, Duty Cycle = ");
+    Serial.println(dutyCycle);
+    setSpeed();
 }
 
 
 void loop(void)
 {
-    Serial.setTimeout(2000);
-    long int n = Serial.parseInt();
+    Serial.setTimeout(1000);
+    long int n = Serial.parseInt(); // hangs for 1sec according to timeout
     if (n != 0 && n <= 70) {
-
 	if (n < 0)
 	    dutyCycle=0;
 	else
 	    dutyCycle=n;
-	
-	myee.pwm=dutyCycle;
-	EEPROM.put(0,myee);
-
+	writeEEPROM();
 	Serial.print("PWM Fan, Duty Cycle = ");
 	Serial.println(dutyCycle);
     }
-    Timer1.pwm(TIMER1_A_PIN, dutyCycle * 1024 / 100);
-    delay(100);
-//  }
+    setSpeed();
+    digitalWrite(LED_BUILTIN, led = !led);
 }
